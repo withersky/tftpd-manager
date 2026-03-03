18ba7ec75d4d1c66079a42de4f082b3c9850c830
withersky <55928929+withersky@users.noreply.github.com>
2026-03-03 16:23:05 +0300

    Дополнение к 1.1.0 Теперь собираем еще tar.gz.

--- build.sh
+++ build.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-# Путь к файлу control
-CONTROL_FILE="tftpd-manager/DEBIAN/control"
+PKG_DIR="tftpd-manager"
+CONTROL="$PKG_DIR/DEBIAN/control"
 
-# Извлекаем версию из файла control
-VERSION=$(grep "^Version:" "$CONTROL_FILE" | awk '{print $2}')
-PACKAGE_NAME=$(grep "^Package:" "$CONTROL_FILE" | awk '{print $2}')
-ARCH=$(grep "^Architecture:" "$CONTROL_FILE" | awk '{print $2}')
-
-# Формируем имя файла по стандартам Debian: имя_версия_архитектура.deb
-DEB_FILE="${PACKAGE_NAME}_${VERSION}_${ARCH}.deb"
-
-echo "Сборка пакета: $PACKAGE_NAME версии $VERSION..."
-
-# Собираем пакет
-dpkg-deb --build tftpd-manager "$DEB_FILE"
-
-if [ $? -eq 0 ]; then
-    echo "-------------------------------------------"
-    echo -e "\e[32mУспешно! Файл создан: $DEB_FILE\e[0m"
-    echo "-------------------------------------------"
-else
-    echo -e "\e[31mОшибка при сборке пакета.\e[0m"
+if [ ! -f "$CONTROL" ]; then
+    echo "Ошибка: Файл control не найден!"
     exit 1
 fi
+
+NAME=$(grep "^Package:" "$CONTROL" | cut -d' ' -f2)
+VERSION=$(grep "^Version:" "$CONTROL" | cut -d' ' -f2)
+ARCH=$(grep "^Architecture:" "$CONTROL" | cut -d' ' -f2)
+
+DEB_NAME="${NAME}_${VERSION}_${ARCH}.deb"
+TAR_NAME="${NAME}_${VERSION}.tar.gz"
+
+echo "--- Начинаю сборку версии $VERSION ---"
+
+dpkg-deb --build "$PKG_DIR" "$DEB_NAME"
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m[OK] Создан пакет: $DEB_NAME\e[0m"
+else
+    echo -e "\e[31m[FAIL] Ошибка сборки DEB\e[0m"
+fi
+
+tar -czf "$TAR_NAME" -C "$PKG_DIR" usr
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m[OK] Создан архив: $TAR_NAME\e[0m"
+else
+    echo -e "\e[31m[FAIL] Ошибка сборки TAR.GZ\e[0m"
+fi
+
+echo "---------------------------------------"
