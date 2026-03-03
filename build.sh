415928f0fabd58190a709d5960b10cb4b8d586ee
withersky <55928929+withersky@users.noreply.github.com>
2026-03-03 15:47:55 +0300

    1.0.1 Патч в скрипте сборки -> теперь собираем с нормальным именем

--- build.sh
+++ build.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-dpkg-deb --build ./tftpd-manager
+
+# Путь к файлу control
+CONTROL_FILE="tftpd-manager/DEBIAN/control"
+
+# Извлекаем версию из файла control
+VERSION=$(grep "^Version:" "$CONTROL_FILE" | awk '{print $2}')
+PACKAGE_NAME=$(grep "^Package:" "$CONTROL_FILE" | awk '{print $2}')
+ARCH=$(grep "^Architecture:" "$CONTROL_FILE" | awk '{print $2}')
+
+# Формируем имя файла по стандартам Debian: имя_версия_архитектура.deb
+DEB_FILE="${PACKAGE_NAME}_${VERSION}_${ARCH}.deb"
+
+echo "Сборка пакета: $PACKAGE_NAME версии $VERSION..."
+
+# Собираем пакет
+dpkg-deb --build tftpd-manager "$DEB_FILE"
+
+if [ $? -eq 0 ]; then
+    echo "-------------------------------------------"
+    echo -e "\e[32mУспешно! Файл создан: $DEB_FILE\e[0m"
+    echo "-------------------------------------------"
+else
+    echo -e "\e[31mОшибка при сборке пакета.\e[0m"
+    exit 1
+fi
